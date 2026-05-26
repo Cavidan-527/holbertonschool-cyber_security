5246f449b683587a07701d9fdcd22916f5f16e2b
Cavidan-527 <13763@holbertonstudents.com>
2026-05-26 15:23:07 -0400

    salam

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --show "$1" | head -n -2 | cut -d: -f2 > 4-password.txt $(john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1)
+john --wordlist=/usr/share/wordlists/rockyou.txt --pot=4-password.txt --format=raw-sha256 "$1"
