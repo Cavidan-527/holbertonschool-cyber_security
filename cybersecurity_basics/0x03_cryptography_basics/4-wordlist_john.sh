e495011ab810048eecb8b0141ae6b9c13ff3b151
Cavidan-527 <13763@holbertonstudents.com>
2026-05-26 15:38:17 -0400

    salam

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 $1
+john --wordlist=/usr/share/wordlists/rockyou.txt --config=/dev/null --pot=4-password.txt --format=raw-sha256 "$1"
